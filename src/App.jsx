ffec2dbd0b14a7cf2606de98ef155f8903d1158b
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-07-05 15:55:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav className='flex flex-col'>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+      </nav>
+    </>
+  )
+}
+
+export default App
